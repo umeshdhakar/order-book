383f8acbeaef6c7283babca7b1df90b1a37e39aa
umeshdhakar <kumar886umesh@gmail.com>
2017-10-13 16:45:06 +0530

    responses shown

--- app/controllers/customerOrder.controller.js
+++ app/controllers/customerOrder.controller.js
@@ -10,34 +10,29 @@
         var orders = this;
         var customerID = 0 ;
         orders.newOrder = {};
+        orders.orderMessage = '';
         $rootScope.$on('showCustomerOrders', function (event, args) {
             var promise = CustomerSectionService.getCustomerOrders(args.id);
             customerID = args.id;
             orders.isCustomerSelected = true ;
             promise.then(function (response) {
                 orders.list = response.data;
-            }).catch(function (error) {});
+            });
             
             promise = CustomerSectionService.getCustomerDetail(args.id);
             promise.then(function (response) {
                 orders.customerData = response.data;
-            }).catch(function (error) {});
+            });
         });
         orders.addNewOrder = function() {
             orders.newOrder.customer = customerID ;
-//            var d = new Date();
-//                var date = d.getDate();
-//                var mon = d.getMonth() + 1;
-//                var year = d.getFullYear();
-//                date = (date < 10) ? '0' + date : date;
-//                mon = (mon < 10) ? '0' + mon : mon;
-//                orders.newOrder.order_date = (year + '-' + mon + '-' + date) ;
-//                orders.newOrder.due = orders.newOrder.total - orders.newOrder.discount - orders.newOrder.advance;
-//                if(orders.newOrder.due > 0 ) orders.newOrder.payment = 'due';
-//                else orders.newOrder.payment = 'received';
             var promise = CustomerSectionService.addNewOrder(orders.newOrder);
             promise.then(function(response){
-                console.log(response.statusText);
+                orders.orderMessage = response.statusText;
+                $rootScope.$broadcast('showCustomerOrders', {id:customerID});
+            })
+            .catch(function(error){
+                orders.orderMessage = error.statusText;
             });
         }
     }
--- app/controllers/orderDetail.controller.js
+++ app/controllers/orderDetail.controller.js
@@ -16,24 +16,12 @@
                 console.log('something wrong');
             });
         orderDetail.updateOrder = function () {
-//            if (orderDetail.data.payment = 'received') {
-//                orderDetail.data.due = 0;
-//            }
-//            if (orderDetail.data.status = 'delievered') {
-//                var d = new Date();
-//                var date = d.getDate();
-//                var mon = d.getMonth();
-//                var year = d.getFullYear();
-//                date = (date < 10) ? '0' + date : date;
-//                mon = (mon < 10) ? '0' + mon : mon;
-//                orderDetail.data.delievery_date = (year + '-' + mon + '-' + date) ;
-//            }
             var promise = OrderDetailService.updateOrder(orderDetail.data);
             promise.then(function (response) {
-                    console.log(response.statusText);
+                    orderDetail.message = response.statusText;
                 })
                 .catch(function (error) {
-                    console.log('something wrong');
+                    orderDetail.message = response.statusText;
                 });
         }
     }
